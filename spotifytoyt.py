6a5ba9eca1c0b311a7ff1d0182bd52b4321b3f42
itsnotkx <swarmpertpro@gmail.com>
2023-12-26 21:07:30 +0800

    cleanup
    added .env files, sessions

--- spotifytoyt.py
+++ spotifytoyt.py
@@ -2,63 +2,81 @@ from flask import Flask,request, redirect, url_for, session
 import requests
 import base64
 import urllib.parse
+from dotenv import load_dotenv
+import os
+import configparser
 
 
 #improvements:
-#1: client secret, client id, and all sensitive data must be stored in a seperate .env(environment) file, stored locally. to push to github, create a placeholder env file
-#   -.env files contain key-value pairs
 #2: move error handling and all commonly used functions into a utils module, which stores commonly used fucntions across the program/
+#store parameters and values that tell the program how to run inside a config folder
+#figure out how to properly accept inputs
+#3.create a SERVER SIDE session with redis and flask
 
-client_id="" 
-client_secret=""
+
+load_dotenv()
+
+
+config=configparser.ConfigParser()
+
 
 app=Flask(__name__)
 
-app.secret_key=""
-@app.route('/')
-def login():
-    authdict={
-        "scope":"playlist-read-private",
-        "client_id": client_id,
-        "response_type":"code",
-        "redirect_uri":'http://localhost:8888/callback',
-        "show_dialog":True
-        
-    }
 
-    authorization_url="https://accounts.spotify.com/authorize?"
+app.secret_key=os.getenv("session_secret_key")
+
+
+config.read("config.ini")
+
+
+@app.route('/')
+def login(): 
+    spotify_client_id=os.getenv("spotify_client_id")
+    authdict={
+        "scope":config["spotifyauth"]["scope"],
+        "client_id":spotify_client_id,
+        "response_type":config["spotifyauth"]["response_type"],
+        "redirect_uri":config["spotifyauth"]["redirect_uri"],
+        "show_dialog":config["spotifyauth"]["show_dialog"],
+    }
+    authorization_url=config["spotifyauth"]["authorization_url"]
     auth_link = authorization_url + urllib.parse.urlencode(authdict)
     return redirect (auth_link)
-    
-def access_token_obtain(code):
+
+
+@app.route("/callback")
+def callback():
+    session["code"] = request.args.get('code')
+    if session["code"] is not None:
+        return redirect ("/spotify_access_token_exchange")
+
+
+@app.route("/spotify_access_token_exchange")
+def access_token_obtain():
+    spotify_client_id=os.getenv("spotify_client_id") #dont store this into variables
+    spotify_client_secret=os.getenv("spotify_client_secret")
     data={
-        "grant_type":"authorization_code",
-        "code":code,
-        "redirect_uri":"http://localhost:8888/callback"
-    }
-
+        "grant_type":config["spotifycodeexchange"]["grant_type"],
+        "code":session["code"],
+        "redirect_uri":config["spotifycodeexchange"]["redirect_uri"] #reminder: redirect uri for EXCHANGING ACCESS TOKEN FOR CODE and AUTHORIZATION has to be the SAME
+        }
     headers={
-        'Authorization':"Basic "+base64.b64encode((client_id+":"+client_secret).encode('utf-8')).decode('utf-8'), #dont concatenate and base64 encode like this, will run into errors especially with special characters in the string.
-
-    }
-
-    #base64.b64encode expects input in bytes, so encode(utf-8) converts client_id+':'+client_secret' into bytes
-    #after that, as header expects values in string format, the whole output of base64.b64encode is decoded using .decode('utf-8')
-
+        'Authorization':"Basic "+base64.b64encode((spotify_client_id+":"+spotify_client_secret).encode('utf-8')).decode('utf-8')
+        #change this to properly concatenate it
+        }
     r=requests.post('https://accounts.spotify.com/api/token',headers=headers,data=data)
     if r.status_code == 200:
-        token = r.json()["access_token"]
-        return token
+        session["spotify_access_token"] = r.json()["access_token"]
+        return redirect ("/playlist")
     else:
-        print ("error 2, code  " + str(r.status_code)+","+str(r.json()["error_description"])) #create a new utils module, put all error handling, or in general commonly used functionality throughout the app/
+        print ("error 2, code  " + str(r.status_code)+","+str(r.json()["error_description"]))
         return "An error has occured"
 
 
 @app.route("/playlist")
 def playlist():
-    token = request.args.get('token')
-    if token:
-        headers={'Authorization':'Bearer '+ token}
+    if session["spotify_access_token"] is not None:
+        headers={'Authorization':'Bearer '+ session["spotify_access_token"]}
         playlists=requests.get('https://api.spotify.com/v1/me/playlists?limit=50',headers=headers)
         if playlists.status_code == 200:
             data=playlists.json()
@@ -74,9 +92,12 @@ def playlist():
                 print (playlist_name_list+", URL= "+playlist_url_list)
                 overall_list.append(playlist_url_list)
             desired_playlist=input("Select your desired playlist from the items above, by copy pasting the desired playlist link here: ")
+
+            #add html here and buttons so that no user will copy paste link wrongly
+
             if desired_playlist in overall_list:
-                desired_id=playlistdata[desired_playlist]
-                return redirect(url_for('extract',desired_id=desired_id,token=token))
+                session["desired_id"]=playlistdata[desired_playlist]
+                return redirect(url_for('extract',desired_id=session["desired_id"],token=session["spotify_access_token"]))
             else:
                 return ("Error: desired playlist link is incorrect.")
         else:
@@ -84,6 +105,7 @@ def playlist():
     else: 
         return "Access token not provided"
     
+
 @app.route("/extract/<desired_id>")
 def extract(desired_id):
     token = request.args.get('token')
@@ -97,7 +119,6 @@ def extract(desired_id):
             x=0
             for i in songjson["items"]:
                 songlist.append(songjson["items"][x]["track"]["name"]+" by "+songjson["items"][x]["track"]["artists"][0]["name"])
-                #songlist.append(songjson["items"][x]["track"]["popularity"])
                 x+=1
             session["songlist"]=songlist
             print (session)
@@ -108,88 +129,83 @@ def extract(desired_id):
     else:
         return "Error: Access token not provided"
 
-youtube_client_id=""
-youtube_client_secret=""
 
 @app.route("/youtubeauth")
 def youtubeauth():
+    youtube_client_id=os.getenv("youtube_client_id")
     yt_auth_dict={
-        "scope":"https://www.googleapis.com/auth/youtube",
+        "scope":config["youtubeauth"]["scope"],
         "client_id":youtube_client_id,
-        "response_type":"code",
-        "redirect_uri":"http://localhost:8888/tokenexchange",
-        "prompt":"consent"
+        "response_type":config["youtubeauth"]["response_type"],
+        "redirect_uri":config["youtubeauth"]["redirect_uri"],
+        "prompt":config["youtubeauth"]["prompt"]
     }
     yt_auth_url="https://accounts.google.com/o/oauth2/auth?"+urllib.parse.urlencode(yt_auth_dict)    
     return redirect (yt_auth_url)
 
+
 @app.route("/tokenexchange")
 def tokenexchange():
-
-    ytcode = request.args.get('code')
-    if ytcode:
-        #obtaining access token for youtube
-        print(ytcode)
+    youtube_client_id=os.getenv("youtube_client_id")
+    youtube_client_secret=os.getenv("youtube_client_secret")
+    session["yt_access_code"] = request.args.get('code')
+    if session["yt_access_code"] is not None:
         params = {
-            'code': ytcode,
+            'code': session["yt_access_code"],
             'client_id': youtube_client_id,
             'client_secret': youtube_client_secret,
-            'redirect_uri': 'http://localhost:8888/tokenexchange',
-            'grant_type': 'authorization_code'
+            'redirect_uri': config["youtubeCodeExchange"]["redirect_uri"],
+            'grant_type': config["youtubeCodeExchange"]["grant_type"]
             }
-        
         yttokenrequest=requests.post('https://oauth2.googleapis.com/token',data=params)
         if yttokenrequest.status_code == 200:
-            response=yttokenrequest.json()
-            yt_token=response["access_token"]
-            return redirect("/createplaylist?token="+yt_token)
+            session["yt_token"]=yttokenrequest.json()["access_token"]
+            #yt_token=response["access_token"]
+            return redirect("/createplaylist?token="+session['yt_token'])
         else:
             print ("error 5, code  " + str(yttokenrequest.status_code)+","+str(yttokenrequest.json()["error_description"]))
             return "An error has occured"
     else:
         return ("Authorization code not obtained")
 
-apikey=""
-    
+
 @app.route("/createplaylist")
 def createplaylist():
-    yt_token=request.args.get("token")
     playlisttitle=input("Enter desired title for playlist here: ")
     params={
         "part":"snippet",
-        "key":apikey
-    }
+        "key":os.getenv("google_api_key")
+        }
     data={
         "snippet":{"title":playlisttitle}
-    }
+        }
     headers={
-        "Authorization":"Bearer "+yt_token
-    }
+        "Authorization":"Bearer "+session["yt_token"]
+        }
     print (data)
     endpoint="https://www.googleapis.com/youtube/v3/playlists?"
     createplaylistrequest=requests.post(url=endpoint,json=data,headers=headers,params=params)
     if createplaylistrequest.status_code==200:
         session["playlistid"]=createplaylistrequest.json()["id"]
-        return redirect ("/songfinder?token="+yt_token)
+        return redirect ("/songfinder?token="+session["yt_token"])
     else:
         return createplaylistrequest.json()
     
+
 @app.route("/songfinder")
 def songfinder():
     videoidlist=[]
-    yt_token=request.args.get("token")
     list1=session["songlist"]
     for song in list1:
         params={
             "q":song,
             "part":"snippet",
             "maxResults":1,
-            "key":apikey,
+            "key":os.getenv("google_api_key"),
             "type":"video",
         }
-
         headers={
-            "Authorization":"Bearer "+yt_token
+            "Authorization":"Bearer "+session["yt_token"]
         }
         r=requests.get("https://www.googleapis.com/youtube/v3/search",params=params, headers=headers)
         response=r.json()
@@ -197,11 +213,11 @@ def songfinder():
             videoid = response['items'][0]['id']['videoId']
             videoidlist.append(videoid)
             session["videoidlist"]=videoidlist
-    return redirect("/updateplaylist?token="+yt_token)
+    return redirect("/updateplaylist?token="+session["yt_token"])
+
 
 @app.route("/updateplaylist")
 def updateplaylist():
-    yt_token=request.args.get("token")
     videoidlist=session["videoidlist"]
     i=0
     for item in videoidlist:
@@ -209,8 +225,8 @@ def updateplaylist():
             "part":"snippet"
         }
         headers={
-            "Authorization":"Bearer "+yt_token,
-            "key":apikey
+            "Authorization":"Bearer "+session["yt_token"],
+            "key":os.getenv("google_api_key")
         }
         data={
             "snippet":{"playlistId":str(session["playlistid"]),
@@ -219,29 +235,14 @@ def updateplaylist():
                         "kind":"youtube#video"}
                     }
         }
-
         r=requests.post("https://www.googleapis.com/youtube/v3/playlistItems",json=data,params=params,headers=headers)
         if r.status_code==200:
             print ("Song added successfully")
         else:
             print(r.json())
         i+=1
-
     return redirect("https://www.youtube.com/playlist?list="+str(session["playlistid"]))
         
-
-@app.route("/callback")
-def callback():
-    code = request.args.get('code')
-    if code:
-        token=access_token_obtain(code)
-        if token:
-            return redirect("/playlist?token="+token)
-        else:
-            return "Failed to obtain access token"
-    #if token and code:
-        #return redirect
-    return "No code parameter provided"
 
 if __name__=="__main__":
     app.run('localhost', 8888)
